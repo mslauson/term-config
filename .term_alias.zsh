aa850011f616c3d59421be84ec11e0b24b544570
Matthew Slauson <matthew@slauson.io>
2023-08-12 13:01:48 -0500

    UD

--- .term_alias.zsh
+++ .term_alias.zsh
@@ -94,7 +94,8 @@ alias redisIntra="redis-cli -h $REDIS_HOST_INTRA -p $REDIS_PORT_INTRA -a $REDIS_
 #jq
 alias jqBwCred="jq -C '[.[] | {name: .name, passwrd: .login.password}]'"
 
-alias yubiNano="gpg-connect-agent 'scd 24009623' 'learn --force' /bye"
+alias yubiNanoC="gpg-connect-agent 'scd 24009623' 'learn --force' /bye"
+alias yubiNanoA="gpg-connect-agent 'scd 22748289' 'learn --force' /bye"
 alias yubiNfc="gpg-connect-agent 'scd 24747238' 'learn --force' /bye"
 
 
