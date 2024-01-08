4f16dfec08f731cefc24e8c8229a2dc004a8673a
Matthew Slauson <matthew@slauson.io>
2024-01-08 12:49:30 -0600

    make commons

---
+++ zsh/keybinds_common.zsh
@@ -0,0 +1,12 @@
+#!/usr/bin/zsh
+
+# Bindings for Ctrl + Left and Ctrl + Right
+bindkey '\e[1;5D' backward-word  # Ctrl + Left
+bindkey '\e[1;5C' forward-word  # Ctrl + Right
+
+# Bindings for Home and End
+bindkey '\e[1;9D' beginning-of-line  # Home
+bindkey '\e[1;9C' end-of-line  # End
+
+#Delete Key
+bindkey '\e[3~' delete-char
---
+++ zsh/zshrc_common.zsh
@@ -0,0 +1,10 @@
+
+
+[ -f "${XDG_DATA_HOME:-$HOME/.local/share}/zap/zap.zsh" ] && source "${XDG_DATA_HOME:-$HOME/.local/share}/zap/zap.zsh"
+
+source ~/.zsh/themes/catppuccin_mocha-zsh-syntax-highlighting.zsh
+
+# completions
+if [ -d $HOME/.zsh/comp ]; then
+    export FPATH="$HOME/.zsh/comp:$FPATH"
+fi
