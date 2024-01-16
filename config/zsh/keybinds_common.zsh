c59fd1383b1a8850fde762a37e0741b805f8aeb5
Matthew Slauson <matthew@slauson.io>
2024-01-16 11:20:44 -0600

    update

--- config/zsh/keybinds_common.zsh
+++ config/zsh/keybinds_common.zsh
@@ -1,9 +1,11 @@
 #!/usr/bin/zsh
 
 # Bindings for Ctrl + Left and Ctrl + Right
-bindkey '\e[1;5D' backward-word  # Ctrl + Left
-bindkey '\e[1;5C' forward-word  # Ctrl + Right
+# bindkey '\e[[1;5D' backward-word  # Ctrl + Left
+# bindkey '\e[[1;5C' forward-word  # Ctrl + Right
 
+bindkey '^[[1;5D' backward-word
+bindkey '^[[1;5C' forward-word
 # Bindings for Home and End
 bindkey '\e[1;9D' beginning-of-line  # Home
 bindkey '\e[1;9C' end-of-line  # End
