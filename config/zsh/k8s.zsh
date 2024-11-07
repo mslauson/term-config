0ac806cdef33456831a0297e12395656646c9d83
Matthew Slauson <matthew@slauson.io>
2024-11-07 03:37:58 -0600

    ud

--- config/zsh/k8s.zsh
+++ config/zsh/k8s.zsh
@@ -2,8 +2,8 @@
 export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
 
  copy-k8s-secret(){
-  kubectl get secret $1 -n $2 -o json \
+  kubectl get secret $3 -n $1 -o json \
  | jq 'del(.metadata["namespace","creationTimestamp","resourceVersion","selfLink","uid"])' \
- | kubectl apply -n $3 -f -
+ | kubectl apply -n $2 -f -
 
 }
